af9c14c9d8a4ba5469db024881b4ae51f86a2b5c
Omar Cardenas <omar.cardenas@tempedge.com>
2020-12-18 15:22:15 -0500

    Starting with MongoDB

--- app.js
+++ app.js
@@ -1,12 +1,14 @@
 import express from 'express';
 import { graphqlHTTP } from 'express-graphql';
+
 import userSchema from './schema/schema.js';
+import testSchema from './schema/types_schema.js';
 
 const app = express();
 
 app.use('/graphql', graphqlHTTP({
     graphiql: true,
-    schema: userSchema
+    schema: testSchema
 }));
 
 app.listen(4000, () => {
---
+++ schema/types_schema.js
@@ -0,0 +1,64 @@
+import {
+    GraphQLObjectType,
+    GraphQLSchema,
+    GraphQLString,
+    GraphQLID,
+    GraphQLBoolean,
+    GraphQLInt,
+    GraphQLFloat, 
+    GraphQLNonNull
+} from 'graphql';
+
+// Scalar Type
+/* 
+    String
+    int
+    Float
+    Boolean
+    ID
+*/
+
+const Person = new GraphQLObjectType({
+    name: 'Person',
+    description: 'Represent a Person Type',
+    fields: () => ({
+        id: { type: GraphQLID },
+        name: { type: new GraphQLNonNull(GraphQLString) },
+        age: { type: new GraphQLNonNull(GraphQLInt) },
+        isMarried: { type: GraphQLBoolean },
+        gpa: { type: GraphQLFloat },
+
+        justAType: {
+            type: Person,
+            resolve(parent, args) {
+                return parent;
+            }
+        }
+    })
+});
+
+const RootQuery = new GraphQLObjectType({
+    name: 'RootQueryType',
+    description: 'Descripcion',
+    fields: {
+        person: {
+            type: Person,
+            resolve(parent, args) {
+                let personObj = {
+                    name: 'Antonio',
+                    age: null,
+                    isMarried: true,
+                    gpa: 4.0
+                }           
+                
+                return personObj;
+            }
+        }
+    }
+});
+
+const testSchema = new GraphQLSchema({
+    query: RootQuery,
+});
+
+export default testSchema;
